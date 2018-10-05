00f14da16f0dbd374dc6360190d868e99ec69c76
Jack53416 <jck53416@gmail.com>
2018-10-05 15:44:47 -0500

    Add main entry for the worker script

---
+++ localizator/main.py
@@ -0,0 +1,22 @@
+import sys
+from twisted.python import log
+from localizator.sensor_matrix import SensorMatrix
+from localizator.connection import Connection, App
+
+sensorMat = SensorMatrix([
+    (1, 0, 0),
+    (0, 0, 0),
+    (0, 2, 0),
+    (0, 0, 3)
+])
+
+
+def __main__():
+    log.startLogging(sys.stdout)
+    App.onSimulate = sensorMat.simulate_wave_propagation
+    App.onSettings = sensorMat.update_receiver_pos
+    connection = Connection()
+    connection.run()
+
+
+__main__()
