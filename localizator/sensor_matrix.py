c6ffa39e002d070009ad7f6419d8c05d8f8a127a
Jack53416 <jck53416@gmail.com>
2019-01-16 12:13:16 -0600

    Rework debug history

--- localizator/sensor_matrix.py
+++ localizator/sensor_matrix.py
@@ -3,8 +3,9 @@ import serial
 import struct
 from bisect import bisect_left
 from collections import deque
-from typing import Tuple, List
-from localizator.receiver import Receiver
+from typing import Tuple, List, NamedTuple, Sized, Iterable
+import itertools
+from localizator.receiver import Receiver, SliceDeck
 from localizator.dft import DFT
 from localizator.MLE import MLE
 from localizator.math_tools import gcc_phat
@@ -14,8 +15,62 @@ import matplotlib.pyplot as plt
 import librosa
 import librosa.display
 
-import scipy.io.wavfile
-import scipy.signal
+
+class HistoryEvent(object):
+    def __init__(self, start_idx: int, end_idx: int, result: List[np.ndarray]):
+        self.start_idx = start_idx
+        self.end_idx = end_idx
+        self.result = result
+
+    def decrement_indexes(self, value) -> bool:
+        self.start_idx -= value
+        self.end_idx -= value
+        return self.start_idx < 0
+
+
+class DebugHistory(object):
+
+    def __init__(self, data_chunk: int, buffer_size: int):
+        self.data_buffer = SliceDeck(maxlen=buffer_size)
+        self._data_chunk = data_chunk
+        self._events: List[HistoryEvent] = []
+        self._time_offset = 0
+
+    def extend_data(self, data: Sized):
+        if len(self.data_buffer) + len(data) > self.data_buffer.maxlen:
+            self._time_offset += len(data)
+
+        self.data_buffer.extend(data)
+
+    def append_event(self, overall_idx: int, s_idx, e_idx: int, result: List[np.ndarray]):
+
+        if e_idx > self._data_chunk:
+            overall_idx -= 1
+            s_idx -= self._data_chunk
+            e_idx -= self._data_chunk
+
+        l_bound = overall_idx * self._data_chunk + s_idx
+        u_bound = overall_idx * self._data_chunk + e_idx
+
+        self._events.append(HistoryEvent(l_bound, u_bound, result))
+
+    def plot(self, env_history: np.ndarray = np.array([])):
+            time_axis = range(self._time_offset, self._time_offset + len(self.data_buffer))
+            plt.figure(figsize=(18, 10))
+            plt.plot(time_axis, np.array(self.data_buffer), 'b.-')
+            plt.axhline(y=12000)
+            plt.axhline(y=7000)
+
+            for event in self._events:
+                if event.start_idx >= self._time_offset:
+                    plt.axvspan(event.start_idx, event.end_idx, facecolor='#2ca02c', alpha=0.5)
+
+            plt.plot(time_axis, np.array(env_history), 'r')
+            plt.tight_layout(rect=[0.02, 0.03, 1, 0.95])
+            plt.xlabel("Sample number", fontsize=20)
+            plt.ylabel("ADC value", fontsize=20)
+            plt.savefig("signal.png")
+            plt.show()
 
 
 class SensorMatrix(object):
@@ -26,17 +81,18 @@ class SensorMatrix(object):
     def __init__(self,
                  receiver_coords: List[Tuple[float, float, float]],
                  reference_rec_id: int = 0,
-                 rec_buff_size: int = 2,
+                 rec_buff_size: int = 4096 * 2,
                  sampling_freq: int = 41666,
                  data_chunk: int = 4096,
                  debug: bool = False):
 
         receivers: List[Receiver] = [Receiver(rec[0], rec[1], rec[2], buffer_size=rec_buff_size)
                                      for rec in receiver_coords]
-        self._sound_detector = SoundDetector(0.9993)
-        self._mle_calc = MLE(receivers, reference_rec_id=reference_rec_id)
+        debug_buff_size = 120 * data_chunk
+        self._sound_detector = SoundDetector(0.9993, debug_buff_size)
+        self._mle_calc = MLE(receivers, src_conditions=lambda src: 0 <= src[2] < 3.0, reference_rec_id=reference_rec_id)
         self._data_chunk = 4096
-        self._dft = DFT(256, sampling_freq)
+        self._dft = DFT(512, sampling_freq)
         self._rec_dft_buff = np.array([])
         self._serial_settings = {
             "channelNr": 4,
@@ -51,16 +107,12 @@ class SensorMatrix(object):
             "minPart": 0.05,
             "noiseFloor": 5000
         }
+
         self.debug = debug
 
-        self._memory_buff = []
-        self._mem_buff_limit = sampling_freq * 30
-        self._evt_chunk_idxs = []
-        fs, self._reference = scipy.io.wavfile.read("samples/referenceBounce.wav")
+        self.debug_history = DebugHistory(data_chunk, debug_buff_size)
 
-        self._hit_ctr = 0
-
-    def start_cont_localization(self, input_src: str ="serial", filename="input.wav"):
+    def start_cont_localization(self, input_src: str = "serial", filename="input.wav"):
         byte_count = self._serial_settings["channelNr"] * self._data_chunk * self._serial_settings["resultSize_bytes"]
         Receiver.isSimulation = False
 
@@ -74,18 +126,10 @@ class SensorMatrix(object):
 
                 for idx in range(0, length):
                     input_bytes = wav.readframes(self._data_chunk)
-                    if self.localize(input_bytes) and self.debug:
-                        self._evt_chunk_idxs.append(idx)
+                    self.localize(input_bytes, idx)
 
-                if len(self._evt_chunk_idxs) >= 0:
-                    #plt.subplot(311)
-                    plt.plot(self._memory_buff, 'b.-')
-                    for idx in self._evt_chunk_idxs:
-                        plt.axvspan(idx * self._data_chunk, (idx + 1) * self._data_chunk - 1,
-                                    facecolor='#2ca02c', alpha=0.5)
-                    plt.plot(self._sound_detector.env_history, 'r')
-                    plt.tight_layout()
-                    plt.show()
+                if self.debug:
+                    self.debug_history.plot(self._sound_detector.env_history)
         else:
             with serial.Serial(self._serial_settings["port"],
                                self._serial_settings["baud"],
@@ -94,7 +138,7 @@ class SensorMatrix(object):
                     input_bytes = ser.read(byte_count)
                     self.localize(input_bytes)
 
-    def localize(self, raw_data: bytes) -> bool:
+    def localize(self, raw_data: bytes, idx: int = 0):
         """Performs the whole localization process: check for searched signal, and if it is found calculate the
         src position, returns true if it was detected and false otherwise(for statistics)"""
         byte_len = len(raw_data)
@@ -102,26 +146,36 @@ class SensorMatrix(object):
                                           raw_data)
         recs = self._mle_calc.receivers
         energy = []
+
         # Split data into separate channels
         for ch_id in range(0, self._serial_settings["channelNr"]):
             ch_data = np.array(frames[ch_id::self._serial_settings["channelNr"]], dtype=np.float32)
             energy.append(sum(map(lambda x: x * x, ch_data)))
-            recs[ch_id].data_buffer.append(ch_data)
+            recs[ch_id].data_buffer.extend(ch_data)
 
         # claculate energy of all channels na choose the strongest
         strongest_idx: int = np.argmax(energy)
 
-        is_event, s_idx, e_idx = self.is_event_detected(recs[strongest_idx].data_buffer, strongest_idx)
+        if self.debug:
+            self.debug_history.extend_data(recs[strongest_idx].data_buffer[self._data_chunk::])
 
-        if is_event:
-            # find TdoA
-            self.calculate_tdoa(s_idx, e_idx)
-            # calculate src
-            res = self.estimate_src_position()
-            print("calculation result:{}".format(res))
-            # send to server
-            return True
-        return False
+        signal_buffer = recs[strongest_idx].data_buffer
+
+        self._sound_detector.detect_sound(signal_buffer, 12000, 7000, data_offset=self._data_chunk)
+
+        while len(self._sound_detector.events) > 0:
+            l_idx, h_idx, s_mic = self._sound_detector.events.pop()
+
+            is_event = self.is_event_detected(signal_buffer[l_idx: h_idx])
+
+            if is_event:
+                # find TdoA
+                self.calculate_tdoa(l_idx, h_idx)
+                # calculate src
+                res = self.estimate_src_position()
+                print("calculation result:{}".format(res))
+                self.debug_history.append_event(idx, l_idx, h_idx, res)
+                # send to server
 
     def update_receiver_pos(self, positions: List[Tuple[float, float, float]], ref_id: int = 0):
         """Updates the spatial positions of all microphones connected to the array. If less than 4 new positions are
@@ -139,83 +193,60 @@ class SensorMatrix(object):
     def get_raw_data(self):
         pass
 
-    def is_event_detected(self, signal_buffer: deque, mic_id: int) -> Tuple[bool, int, int]:
+    def is_event_detected(self, sound_signal: Iterable) -> bool:
         """Detects if the ping pong ball hit was registered. This is done in a simple fashion by taking into account
            only frequencies from certain range specified in recognition settings. Then the spectrum is calculated and
            smoothed by moving average and compared via normalized cross-correlation to the saved sound pattern"""
-        last_idx = len(signal_buffer) - 1
-        current_signal = signal_buffer[last_idx]
 
-        if self.debug and len(self._memory_buff) < self._mem_buff_limit:
-            self._memory_buff += current_signal.tolist()
+        sound_signal = np.array(sound_signal, np.float32)
 
-        self._sound_detector.detect_sound(current_signal, 15000, 12000)
+        D = librosa.stft(sound_signal, n_fft=64)
+        spectrogram = np.abs(D)
+        spectrogram_db = librosa.amplitude_to_db(spectrogram, ref=np.max)
+        frequencies = np.linspace(0, 41666, 33)
+        pos_l = bisect_left(frequencies, self._recognition_settings["lowSpectrum"])
+        pos_h = bisect_left(frequencies, self._recognition_settings["highSpectrum"])
 
-        while len(self._sound_detector.events) > 0:
-            l_idx, h_idx, s_mic = self._sound_detector.events.pop()
+        spec_slice = np.mean(spectrogram_db[pos_l:pos_h, :], axis=0)
+        bounce_idx = [idx for idx, el in enumerate(spec_slice) if el >= -32.0]
 
-            if l_idx >= h_idx:
-                if s_mic != mic_id:
-                    signal_buffer = self._mle_calc.receivers[s_mic].data_buffer
-                    current_signal = signal_buffer[last_idx]
-                prev_signal_chunk = signal_buffer[0]
-                sound_signal = np.concatenate([prev_signal_chunk[l_idx::], current_signal[:h_idx]])
-            else:
-                sound_signal = current_signal[l_idx: h_idx]
+        if len(bounce_idx) >= 3:
+            return True
 
-            D = librosa.stft(sound_signal, n_fft=64)
-            spectrogram = np.abs(D)
-            spectrogram_db = librosa.amplitude_to_db(spectrogram, ref=np.max)
-            frequencies = np.linspace(0, 41666, 33)
-            pos_l = bisect_left(frequencies, self._recognition_settings["lowSpectrum"])
-            pos_h = bisect_left(frequencies, self._recognition_settings["highSpectrum"])
-
-            spec_slice = np.mean(spectrogram_db[pos_l:pos_h, :], axis=0)
-            bounce_idx = [idx for idx, el in enumerate(spec_slice) if el >= -32.0]
-
-            if len(bounce_idx) >= 3:
-                return True, l_idx, h_idx
-
-        return False, -1, -1
+        return False
 
     def calculate_tdoa(self, s_idx: int, e_idx: int):
         """Calculates TDoA between all receivers and reference one in the sensor matrix. Results are stored within
            receiver object"""
 
-        # extract bounce sound and its surronding from rec buffers
+        # extract bounce sound and its surrounding from rec buffers
+        l_bound = s_idx - self._dft.dft_size + 1
+        u_bound = s_idx + self._dft.dft_size - 1
 
-        l_bound = s_idx - self._dft.size//2 + 1
-        u_bound = (s_idx + self._dft.size//2) % (self._data_chunk - 1)
-        last_idx = len(self._mle_calc.ref_rec.data_buffer) - 1
-
-        if s_idx >= e_idx:
-            last_idx -= 1
-
-        if l_bound < 0:
-            l_bound = (self._data_chunk - 1) + l_bound
-
-        if l_bound >= u_bound:
-            bounce_data = [np.concatenate([rec.data_buffer[0][l_bound:], rec.data_buffer[1][:u_bound]])
-                           for rec in self._mle_calc.receivers]
-        else:
-            bounce_data = [rec.data_buffer[last_idx][l_bound: u_bound+1] for rec in self._mle_calc.receivers]
+        bounce_data = [rec.data_buffer[l_bound: u_bound] for rec in self._mle_calc.receivers]
 
         for rec_idx in range(1, self._serial_settings["channelNr"]):
 
             delay, hist = gcc_phat(bounce_data[rec_idx], bounce_data[0], self._dft, phat=True,
                                    delay_in_seconds=True, buffered_dft=False)
             self._mle_calc.receivers[rec_idx].tDoA = delay
-        if self.debug:
+        if not self.debug:
             print(delay)
+            plt.figure(figsize=(18, 10))
             plt.subplot(311)
-            plt.plot(bounce_data[0], label="signal")
-            plt.plot(bounce_data[1], label="delayed")
+            plt.plot(bounce_data[0], label="mic_1")
+            plt.plot(bounce_data[1], label="mic_2")
+            plt.legend()
             plt.subplot(312)
-            plt.plot(bounce_data[0], label="signal")
-            plt.plot(bounce_data[2], label="delayed")
+            plt.plot(bounce_data[0], label="mic_1")
+            plt.plot(bounce_data[2], label="mic_3")
+            plt.legend()
             plt.subplot(313)
-            plt.plot(bounce_data[0], label="signal")
-            plt.plot(bounce_data[3], label="delayed")
+            plt.plot(bounce_data[0], label="mic_1")
+            plt.plot(bounce_data[3], label="mic_4")
+            plt.xlabel("Sample number")
+            #plt.tight_layout(rect=[0.00, 0.03, 1, 0.95])
+            plt.legend()
             plt.show()
 
     def estimate_src_position(self) -> List[np.ndarray]:
